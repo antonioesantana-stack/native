368c39294422c2f71d5a8ad358b978bbba89a8e3
anttonioelias <anthonyosantana2000@gmail.com>
2026-06-17 21:33:24 -0300

    Primeiro commit: Estrutura do app nativo

---
+++ native/meuApp/App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Antonio elias </Text>
+      <text>liguagem favarita, PYTHON </text>
+      <Text> TELEFONE (75) 982320246 </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
